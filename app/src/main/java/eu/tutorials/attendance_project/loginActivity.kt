3f976e30ed0c8a2a33b9e214656affea4118fb35
RoRaut <rohinibraut20@gmail.com>
2024-02-07 19:38:16 +0530

    new1

--- app/src/main/java/eu/tutorials/attendance_project/loginActivity.kt
+++ app/src/main/java/eu/tutorials/attendance_project/loginActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class loginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,7 +14,14 @@ class loginActivity : AppCompatActivity() {
         val button = findViewById<Button>(R.id.Login1)
 
         button.setOnClickListener {
-            val intent = Intent(this, loginActivity::class.java)
+            val intent = Intent(this, homeActivity::class.java)
+            startActivity(intent)
+        }
+
+        val textView = findViewById<TextView>(R.id.btnreg)
+
+        textView.setOnClickListener {
+            val intent = Intent(this, registrationActivity::class.java)
             startActivity(intent)
         }
 
